9a82b4fa2f994977c2a52cd27b7d1be58164d8df
euijin <aghpt04@gmail.com>
2024-09-19 10:53:03 +0900

    🐛 :: 라우터 수정

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,13 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home } from "./Pages/Home";
 
 export const Router = () => {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/ded" element={<div>djeiji</div>} />
-            </Routes>
+            <Routes></Routes>
         </BrowserRouter>
     );
 };
