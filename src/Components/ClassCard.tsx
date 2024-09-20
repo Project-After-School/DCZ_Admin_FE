57c4ed6f3577bd75b93810a28ea183990adb02a0
euijin <aghpt04@gmail.com>
2024-09-20 14:13:37 +0900

    🎨 :: 클래스 카드 퍼블리싱

---
+++ src/Components/ClassCard.tsx
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+import People from "../Assets/People.svg";
+import HomeWork from "../Assets/HomeWork.svg";
+
+export const ClassCard = () => {
+    return (
+        <Wrapper>
+            <ContentWrapper>
+                <img src={People} />
+                <p>2학년 2반</p>
+            </ContentWrapper>
+            <ClassRoomName>클래스룸 이름</ClassRoomName>
+            <Line />
+            <ContentWrapper>
+                <img src={HomeWork} />
+                <p>현재 제출중인 과제</p>
+            </ContentWrapper>
+        </Wrapper>
+    );
+};
+
+const Wrapper = styled.div`
+    display: flex;
+    width: 460px;
+    height: 149px;
+    flex-direction: column;
+    padding: 20px;
+    border: 1px solid #414142;
+    border-radius: 16px;
+    gap: 20px;
+`;
+
+const ContentWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 12px;
+    > p {
+        font-weight: 600;
+        font-size: 20px;
+    }
+`;
+
+const ClassRoomName = styled.p`
+    font-weight: 600;
+    font-size: 24px;
+`;
+
+const Line = styled.div`
+    width: 100%;
+    background-color: #414142;
+    height: 1px;
+`;
