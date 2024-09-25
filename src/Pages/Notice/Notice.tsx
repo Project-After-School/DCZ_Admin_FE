890d127ca4540a70f8abf665f90689d4af02f3b1
hA2zy <t01040545119@gmail.com>
2024-09-25 20:10:07 +0900

    🐛 :: 공지 만들기 버튼 위치

--- src/Pages/Notice/Notice.tsx
+++ src/Pages/Notice/Notice.tsx
@@ -1,17 +1,17 @@
 import styled from "styled-components";
-import { Header } from "../Components/Common/Header";
-import { SubHeader } from "../Components/Common/SubHeader";
-import { NoticeList } from "../Components/NoticeList";
+import { Header } from "../../Components/Common/Header";
+import { SubHeader } from "../../Components/Common/SubHeader";
+import { NoticeList } from "../../Components/Notice/NoticeList";
 
 export const Notice = () => {
   return (
     <Wrapper>
       <Header />
       <TitleWrapper>
-                    <Text>과제 제출 (클래스룸 명)</Text>
-                    <ClassName>2학년 2반</ClassName>
-                </TitleWrapper>
-                <Line />
+        <Text>과제 제출 (클래스룸 명)</Text>
+        <ClassName>2학년 2반</ClassName>
+      </TitleWrapper>
+      <Line />
       <MiddleWrapper>
         <SubHeader />
         <Button>공지 만들기</Button>
@@ -34,40 +34,42 @@ const Wrapper = styled.div`
 `;
 
 const TitleWrapper = styled.div`
-    display: flex;
-    align-items: center;
-    margin-top: 171px;
-    width: calc(1040px - 48px);
+  display: flex;
+  align-items: center;
+  margin-top: 171px;
+  width: calc(1040px - 48px);
+  justify-content: space-between;
 `;
 
 export const Text = styled.p`
-    font-weight: 600;
-    font-size: 42px;
+  font-weight: 600;
+  font-size: 42px;
 `;
 
 export const ClassName = styled.p`
-    font-weight: 600;
-    font-size: 32px;
-    margin-left: auto;
+  font-weight: 600;
+  font-size: 32px;
+  margin-left: auto;
 `;
 
 const Line = styled.div`
-    margin-top: 40px;
-    width: 1040px;
-    height: 1px;
-    background-color: #414142;
-    margin-bottom: 28px;
+  margin-top: 40px;
+  width: 1040px;
+  height: 1px;
+  background-color: #414142;
+  margin-bottom: 28px;
 `;
 
 const MiddleWrapper = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: flex-start;
+  justify-content: space-between;
+  align-items: center;
   width: 1040px;
   margin-bottom: 36px;
 
   @media (max-width: 768px) {
     flex-direction: column;
+    align-items: flex-start;
   }
 `;
 
