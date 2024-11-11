d11c74c87ab462a7dc4d9bd51a43e180ddbff94b
hA2zy <t01040545119@gmail.com>
2024-11-11 17:30:50 +0900

    📑 ♻️ :: 내용 버튼 해결

--- src/Pages/Notice/NoticeUpload.tsx
+++ src/Pages/Notice/NoticeUpload.tsx
@@ -4,84 +4,6 @@ import { Header } from "../../Components/Common/Header";
 import { Input } from "../../Components/Common/Input";
 import { TextArea } from "../../Components/Common/TextArea";
 import { Button } from "../../Components/Common/Button";
-<<<<<<< Updated upstream
-import { CheckClass } from "../../Components/HomeWork/CheckClass";
-
-export const NoticeUpload = () => {
-    return (
-        <Wrapper>
-            <Header />
-            <Container>
-                <TitleWrapper>
-                    <Text>공지 업로드</Text>
-                    <ClassName>2학년 2반</ClassName>
-                </TitleWrapper>
-                <Line />
-                <ContentWrapperContainer>
-                    <ContentWrapper>
-                        <Input label="제목" placeholder="제목을 입력해주세요." />
-                    </ContentWrapper>
-                    <ContentWrapper>
-                        <TextArea label="제출 양식" height="188px" placeholder="제출 양식을 입력해주세요." />
-                    </ContentWrapper>
-                    <CheckClass />
-                </ContentWrapperContainer>
-                <ButtonWrapper>
-                    <Button to="/notice" content="과제 업로드" />
-                </ButtonWrapper>
-            </Container>
-        </Wrapper>
-    );
-};
-
-const Wrapper = styled.div`
-    display: flex;
-    justify-content: center;
-`;
-
-const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-top: 171px;
-    width: calc(1040px - 48px);
-    position: relative;
-`;
-
-const Line = styled.div`
-    margin-top: 40px;
-    width: 100%;
-    height: 1px;
-    background-color: #414142;
-    margin-bottom: 60px;
-`;
-
-const TitleWrapper = styled.div`
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-`;
-
-const ContentWrapperContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 60px;
-    width: 100%;
-`;
-
-const ContentWrapper = styled.div`
-    display: flex;
-`;
-
-const ButtonWrapper = styled.div`
-    position: relative;
-    top: 60px;
-    right: 0;
-    width: 100%;
-    display: flex;
-    justify-content: flex-end;
-    margin-bottom: 100px;
-=======
 import { NoticeApi } from "../../Apis/NoticeApi";
 
 export const NoticeUpload: React.FC = () => {
@@ -229,5 +151,4 @@ const ButtonWrapper = styled.div`
   margin-top: 40px;
   display: flex;
   justify-content: flex-end;
->>>>>>> Stashed changes
 `;
